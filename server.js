5abdd85075033d6e6dfa29943110fc7ca58d11b0
Teshager Mekuria <tinishutmd@gmail.com>
2026-04-01 15:53:50 +0300

    AKSA

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+// MonsterASP.NET usually provides the PORT via environment variables
 const port = process.env.PORT || 3000;
 
 app.prepare().then(() => {
-  createServer((req, req) => {
+  // FIX: Change (req, req) to (req, res) below
+  createServer((req, res) => {
     const parsedUrl = parse(req.url, true)
     handle(req, res, parsedUrl)
   }).listen(port, (err) => {
